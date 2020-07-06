6d7e2761b610f0b3dcf0ffe4f2eb20acb6acbd1a
uladzimir <volodyasn@gmail.com>
2020-07-06 21:48:33 +0300

    Initial commit

---
+++ src/by/sanko/task7/Main.java
@@ -0,0 +1,37 @@
+package by.sanko.task7;
+
+import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class Main {
+    public static void main(String[] args) {
+        for(int i = 0; i < 3; i++) {
+            System.out.println("1 - агклйиский /n 2 - белорусский /n любой - русский");
+            char c = 0;
+            try{
+                c = (char)System.in.read();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+            String country = "";
+            String language = "";
+            switch (i) {
+                case '1' :
+                    country = "US";
+                    language = "EN";
+                    break;
+                case '2' :
+                    country = "BY";
+                    language = "BE";
+                    break;
+            }
+            Locale current = new Locale(language, country);
+            ResourceBundle rb = ResourceBundle.getBundle("property.text",current);
+            String s1 = rb.getString("capital");
+            String s2 = rb.getString("president");
+            System.out.println(s1);
+            System.out.println(s2);
+        }
+    }
+}
